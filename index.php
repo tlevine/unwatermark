73adc340591db0ce0e4ec6a5c8ae2dba2c6e5064
Thomas Levine <perluette@thomaslevine.com>
2012-06-20 15:10:24 -0400

    inotice

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 $out=shell_exec("sed -i '/font color=black.This file is not intended to be viewed directly using a web browser/d' ../*.html");
 echo $out;
+echo 'The watermarks have been removed.';
 ?>
