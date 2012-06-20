3fd09ad6224ee4919c32bb1bb8a283c185764abb
Thomas Levine <perluette@thomaslevine.com>
2012-06-20 15:09:52 -0400

    ifix

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo 'hi';
-$out=shell_exec("cd ..; sed -i '/font color=black.This file is not intended to be viewed directly using a web browser/d' ../*.html");
+$out=shell_exec("sed -i '/font color=black.This file is not intended to be viewed directly using a web browser/d' ../*.html");
 echo $out;
 ?>
