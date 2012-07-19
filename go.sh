599b24e65e86aab31db45072e214754e38aa25e7
Thomas Levine <occurrence@thomaslevine.com>
2012-07-19 17:33:58 +0100

    aoeu

--- go.sh
+++ go.sh
@@ -2,7 +2,13 @@
 
 for file in /srv/www.fadelee.com/*.html
   do
-  cat "$file" | /srv/www.fadelee.com/unwatermark/tail-r.py | sed "3c\ $(cat /srv/www.fadelee.com/unwatermark/piwik.html)" | /srv/www.fadelee.com/unwatermark/tail-r.py > "$file".tmp
+  cat "$file" |
+    /srv/www.fadelee.com/unwatermark/tail-r.py |
+    sed '3 {
+      r /srv/www.fadelee.com/unwatermark/piwik.html
+      d
+    }'
+    /srv/www.fadelee.com/unwatermark/tail-r.py > "$file".tmp
   cat "$file".tmp > "$file"
   rm "$file".tmp
 done
