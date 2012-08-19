1cf8060bf40636af452135a52bbf810858a4ddcb
Thomas Levine <occurrence@thomaslevine.com>
2012-08-19 03:31:47 -0400

    encapsulate things

--- old/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-shell_exec('ssh fadelee@localhost /srv/www.fadelee.com/unwatermark/go.sh');
-echo 'The watermarks have been removed.';
-?>
--- old/tail-r.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-lines=sys.stdin.read().split('\n')
-lines.reverse()
-
-print('\n'.join(lines))
