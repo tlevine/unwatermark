14c2f80e4799269f61202f67e96322118c9f0dc7
Thomas Levine <occurrence@thomaslevine.com>
2012-08-19 02:14:27 -0400

    aoue

--- erase.py
+++ erase.py
@@ -12,22 +12,6 @@ def xpath0(htmlelement, xpath):
 lxml.html.HtmlElement.xpath0 = xpath0
 del(xpath0)
 
-def remove_watermark(html):
-    watermark = html.xpath0(
-      '//div[@style="position:absolute;left:50;top:50;background:white"]'
-      '[font[@color="black"]]'
-    )
-    if watermark is not None:
-        watermark.getparent().remove(watermark)
-        removed = True
-    else:
-        removed = False
-
-    return removed, html
-
-def remove_excludes(html):
-    pass
-
 PIWIK = lxml.html.fromstring('''<!-- Piwik -->
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.thomaslevine.com/" : "http://piwik.thomaslevine.com/");
@@ -53,7 +37,59 @@ def add_piwik(html):
 
     return added, html
 
+def remove_watermark(html):
+    watermark = html.xpath0(
+      '//div[@style="position:absolute;left:50;top:50;background:white"]'
+      '[font[@color="black"]]'
+    )
+    if watermark is not None:
+        watermark.getparent().remove(watermark)
+        removed = True
+    else:
+        removed = False
+
+    return removed, html
+
+def edit_files():
+    removed_watermark = []
+    added_piwik = []
+    for filename in os.listdir(SITE_ROOT):
+        if filename[-5:] != '.html':
+            continue
+
+        absolute_path = os.path.join(SITE_ROOT, filename)
+        f = open(absolute_path, 'r')
+        html = lxml.html.fromstring(f.read())
+        f.close()
+
+        removed_watermark_from_file, html = remove_watermark(html)
+        added_piwik_to_file, html = add_piwik(html)
+
+        if removed_watermark_from_file:
+            removed_watermark.append(filename)
+
+        if added_piwik_to_file:
+            added_piwik.append(filename)
+
+        f = open(absolute_path, 'w')
+        f.write(tostring(html))
+        f.close()
+
+    print 'Removed the watermark from these files:'
+    print '\n  '.join(removed_watermark)
+
+    print 'Added the piwik analytics snippet to these files:'
+    print '\n  '.join(added_piwik)
+
+def main():
+    print 'HTTP/1.1 200 OK'
+    print 'Content-Type: text/plain'
+    print '\n'
+    os.system('cd /srv/www.fadelee.com && git init')
+    os.system('cd /srv/www.fadelee.com && git commit . -m new\ upload')
+    edit_files()
+    os.system('cd /srv/www.fadelee.com && git commit . -m removed\ watermark')
+
+SITE_ROOT = os.path.join('srv', 'www.fadelee.com')
 if __name__ == '__main__':
-    html = lxml.html.fromstring(open('fixtures/has_watermark.html').read())
-    a, b = remove_watermark(html)
-    import pdb; pdb.set_trace()
+    main()
