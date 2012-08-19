8605fc50712d0e36bcf8b121056963cb9aa00bdf
Thomas Levine <occurrence@thomaslevine.com>
2012-08-19 01:19:15 -0400

    keep the comment

--- erase.py
+++ erase.py
@@ -1,10 +1,37 @@
-from lxml.html import fromstring
+import lxml.html
+
+# Single xpath helper
+def xpath0(htmlelement, xpath):
+    results = htmlelement.xpath(xpath)
+    if len(results) == 0:
+        return None
+    elif len(results) == 1:
+        return results[0]
+    else:
+        raise ValueError('The xpath matched more than one node')
+lxml.html.HtmlElement.xpath0 = xpath0
+del(xpath0)
+
+def remove_watermark(html):
+    watermark = html.xpath0(
+      '//div[@style="position:absolute;left:50;top:50;background:white"]'
+      '[font[@color="black"]]'
+    )
+    if watermark is not None:
+        watermark.getparent().remove(watermark)
+        removed = True
+    else:
+        removed = False
+
+    return removed, html
 
 def remove_excludes(html):
     pass
 
-def remove_watermark(html):
-    pass
-
 def add_piwik(html):
     pass
+
+if __name__ == '__main__':
+    html = lxml.html.fromstring(open('fixtures/has_watermark.html').read())
+    a, b = remove_watermark(html)
+    import pdb; pdb.set_trace()
--- tests.py
+++ tests.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python
 import os
 import nose.tools as n
-from lxml.html import fromstring
+import lxml.html
 from erase import remove_excludes, remove_watermark, add_piwik
+
+from doctest import Example
+from lxml.doctestcompare import LXMLOutputChecker
+
+def assert_xml_equal(got, want):
+    got = lxml.html.tostring(got)
+    want = lxml.html.tostring(want)
+    checker = LXMLOutputChecker()
+    if not checker.check_output(want, got, 0):
+        message = checker.output_difference(Example("", want), got, 0)
+        raise AssertionError(message)
 
 def _load_fixture(name):
     f = open(os.path.join('fixtures', name + '.html'))
-    html = fromstring(f.read())
+    html = lxml.html.fromstring(f.read())
     f.close()
     return html
 
 html_orig = _load_fixture('has_watermark')
+
+class TestRemoveWatermark:
+    def test_start_with_watermark(self):
+        'remove_watermark should remove the watermark'
+        removed, html_observed = remove_watermark(html_orig)
+        html_expected =_load_fixture('no_watermark')
+        n.assert_true(removed)
+#       n.assert_equal(html_observed, html_expected)
+        assert_xml_equal(html_observed, html_expected)
+
+    def test_start_without_watermark(self):
+        'remove_watermark should do nothing if the watermark isn\'t there.'
+        html_expected =_load_fixture('no_watermark')
+        removed, html_observed = remove_watermark(html_expected)
+        n.assert_false(removed)
+#       n.assert_equal(html_observed, html_expected)
 
 def test_no_excludes():
     'remove_excludes should remove all excludes'
@@ -18,17 +45,12 @@ def test_no_excludes():
     html_expected =_load_fixture('no_excludes')
     n.assert_equal(html_observed, html_expected)
 
-def test_no_watermark():
-    'remove_watermark should remove the watermark'
-    html_observed = remove_watermark(html_orig)
-    html_expected =_load_fixture('no_watermark')
-    n.assert_equal(html_observed, html_expected)
-
 def test_piwik():
     'add_piwik should add the piwik stuff.'
     html_no_watermark =_load_fixture('no_watermark')
-    html_observed = add_piwik(html_no_watermark)
+    added, html_observed = add_piwik(html_no_watermark)
     html_expected =_load_fixture('piwik')
+    n.assert_true(added)
     n.assert_equal(html_observed, html_expected)
 
 
